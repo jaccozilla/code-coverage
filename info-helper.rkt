a3cd950379c8e4f36ad0689f0a002eefddff006d
Jonathan Walsh <jowalsh@calpoly.edu>
2011-05-13 18:36:29 -0700

    added info-helper.rkt so that tool.rkt and code-coverage.scrbl can use info.rkt's defines. Allowing the update of only info.rkt to also change the button labels and such in tool.rkt along with the documentation to reflect it

---
+++ info-helper.rkt
@@ -0,0 +1,19 @@
+#lang racket
+;Provide the names of labels and other items that are used across info.rkt, tool.rkt, 
+; and code-coverage.scrbl so that to change the value in all 3 locations only info.rkt
+; needs to be updated
+(require setup/getinfo)
+(provide info-look-up 
+         coverage-suffix
+         tool-name
+         button-label
+         open-with-label)
+
+(define info-proc (get-info (list "code-coverage")))
+(define (info-look-up name) (info-proc name (λ () (symbol->string name))))
+
+(define coverage-suffix (info-look-up 'coverage-suffix))
+(define tool-name (info-look-up 'tool-name))
+(define button-label (info-look-up 'button-label))
+(define open-with-label (info-look-up 'open-with-label))
+         
