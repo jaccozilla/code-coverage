e5a0d524a55d85714b2526d8da11b75661b0c2ec
Jonathan Walsh <jowalsh@calpoly.edu>
2011-06-06 23:03:17 -0700

    wrote some of the report

--- test/scroll-bar.rkt
+++ test/scroll-bar.rkt
@@ -4,21 +4,13 @@
 (define choices (list "choice1" "choice2" "choice3" "choice4"))
 ;(define choices (list "choice1" "choice2" "choice3" "choice4" "choice5" "choice6" "choice7" "choice8"))
 
-(define dialog (instantiate dialog% ("dialog")))
-(new message% [parent dialog] [label "here is a long message"])
+;test 1 - has the bug
+(define dialog (instantiate dialog% ("test 1")))
+(new message% [parent dialog] [label "here is a long message for test 1"])
 (define list-box (new list-box% 
                       [label ""]
                       [choices choices]
                       [parent dialog]
                       [style '(multiple)]
-                      ;[min-height (get-listbox-min-height (length choices))]
-                      ;[callback (λ (c e) 
-                      ;            (if (> (length (send list-box get-selections)) 0)
-                      ;                (if (eq? (send e get-event-type) 'list-box-dclick)
-                      ;                    ((button-callback 'open) null null)
-                      ;                    (enable-open-buttons #t))
-                      ;                (enable-open-buttons #f)))
-                      ;          ]
                       ))
-
 (send dialog show #t)
